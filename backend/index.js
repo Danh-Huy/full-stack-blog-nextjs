392eeef4542dd35241e5f37c3bd0896fb0832ab8
Danh-Huy <ledanhhuy133@gmail.com>
2025-08-20 22:50:14 +0700

    add base backend, connect db, basic api

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js"
+import connectDB from "./lib/connectDB.js"
+
+const app = express()
+
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+
+app.listen(3000, () => {
+    connectDB()
+  console.log("Server is running on port 3000")
+})
